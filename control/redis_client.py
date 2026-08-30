e028dc876a92647da07097a056c07700ecd77b33
kvesicga <kvesicga@gmail.com>
2026-08-30 04:10:55 +0200

    feat: add basic get/set method to class RedisClient

--- control/redis_client.py
+++ control/redis_client.py
@@ -6,3 +6,11 @@ class RedisClient:
     def __init__(self, host = '127.0.0.1', port = 6379):
         self.r = redis.Redis(host = host, port = port, decode_responses = True)
 
+    def get(self, key):
+            return self.r.get(key)
+
+    def set(self, key, value):
+        self.r.set(key, value)
+        self.r.publish(CHANNEL_CONTROLS, key)
+
+
