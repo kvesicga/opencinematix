e39b21f9cbccfde9d4faedc9d99ab484d3ecbcd6
kvesicga <kvesicga@gmail.com>
2026-08-30 23:07:12 +0200

    feat: add channel parameter to RedisClient

--- control/redis_client.py
+++ control/redis_client.py
@@ -5,8 +5,19 @@ CHANNEL_CONTROLS = "cp_controls"
 
 
 class RedisClient:
-    def __init__(self, host = "127.0.0.1", port = 6379, db = 0, on_change = None):
-        self.r = redis.Redis(host = host, port = port, db = db, decode_responses = True)
+    def __init__(self,
+                 host = "127.0.0.1",
+                 port = 6379,
+                 db = 0,
+                 channel = CHANNEL_CONTROLS,
+                 on_change = None):
+
+        self.r = redis.Redis(host = host,
+                             port = port,
+                             db = db,
+                             decode_responses = True)
+
+        self.channel = channel
 
         self.local_updates = set()
         self.lock = threading.Lock()
@@ -14,8 +25,9 @@ class RedisClient:
         self.on_change = on_change
 
         self.pubsub = self.r.pubsub()
-        self.pubsub.subscribe(CHANNEL_CONTROLS)
-        self.listener = threading.Thread(target=self._listen, daemon=True)
+        self.pubsub.subscribe(self.channel)
+        self.listener = threading.Thread(target = self._listen,
+                                         daemon = True)
         self.listener.start()
 
     def get(self, key):
@@ -25,7 +37,7 @@ class RedisClient:
         with self.lock:
             self.local_updates.add(key)
         self.r.set(key, value)
-        self.r.publish(CHANNEL_CONTROLS, key)
+        self.r.publish(self.channel, key)
 
     def _listen(self):
         for message in self.pubsub.listen():
