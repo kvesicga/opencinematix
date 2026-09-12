14485216bdcf38db016cef0ad0145188634e8bb8
kvesicga <kvesicga@gmail.com>
2026-09-12 11:32:57 +0200

    test: require cam_init after a mode change

--- tests/test_camera_state.py
+++ tests/test_camera_state.py
@@ -78,3 +78,9 @@ def test_mode_writes_dimensions_separately(camera, clean_db):
 
     assert clean_db.get("width") == "2028"
     assert clean_db.get("height") == "1520"
+
+
+def test_mode_triggers_camera_reinit(camera, clean_db):
+    camera.set("sensor_mode", "2028x1520x12")
+
+    assert clean_db.get("cam_init") is not None
